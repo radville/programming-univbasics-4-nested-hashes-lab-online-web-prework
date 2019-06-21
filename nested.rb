9363b8145aa465e15b1efb3f4cfce3d312c777f5
Laura Radville <laura.radville@gmail.com>
2019-06-21 13:53:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,8 +81,9 @@ def adding_matz
         }
      }
 programmer_hash[:yukihiro_matsumoto] = {known_for: "Ruby", languages: ["LISP", "C"]}
-    
+puts programmer_hash    
 end
+adding_matz
 
 def changing_alan
 	programmer_hash = 
